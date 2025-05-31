4bcb8d69aed8ea61e0e96bee2737c3d51e140b64
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2025-05-31 03:10:37 +0200

    initial commit

---
+++ src/main/java/home/HiveContactApplication.java
@@ -0,0 +1,13 @@
+package home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiveContactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiveContactApplication.class, args);
+	}
+
+}
