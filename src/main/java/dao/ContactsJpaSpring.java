b3f1d73a496a7ebe86407b61cfe5d7853f738437
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2025-05-31 03:43:30 +0200

    added spring tags to ContactsJpaSpring

--- src/main/java/dao/ContactsJpaSpring.java
+++ src/main/java/dao/ContactsJpaSpring.java
@@ -1,9 +1,17 @@
 package dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import model.Contact;
 
 public interface ContactsJpaSpring extends JpaRepository<Contact, Integer> {
+	Contact findByEmail(String email);
+	@Transactional
+	@Modifying
+	@Query("DELETE FROM Contact c WHERE c.email=?1")
+	void deleteByEmail(String email);
 
 }
