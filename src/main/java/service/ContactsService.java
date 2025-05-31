6e87bbccd47ac285c20e276cd0e32c89c3f7fb3c
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2025-05-31 04:07:32 +0200

    Changes in ContactsService

---
+++ src/main/java/service/ContactsService.java
@@ -0,0 +1,13 @@
+package service;
+
+import java.util.List;
+
+import model.Contact;
+
+public interface ContactsService {
+	boolean addContact(Contact contact);
+	List<Contact> getAllContacts();
+	void updateContact(Contact contact);
+	boolean deleteContact(int idContact);
+	Contact getContact(int idContact);
+}
