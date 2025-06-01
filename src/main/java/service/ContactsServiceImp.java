5a4e49accf67fb4f73720414e74f7aabbf5892d2
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2025-06-01 11:17:39 +0200

    New Class ContactsServiceImp

---
+++ src/main/java/service/ContactsServiceImp.java
@@ -0,0 +1,39 @@
+package service;
+
+import java.util.List;
+
+import model.Contact;
+
+public class ContactsServiceImp implements ContactsService {
+
+	@Override
+	public boolean addContact(Contact contact) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Contact> getAllContacts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateContact(Contact contact) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean deleteContact(int idContact) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Contact getContact(int idContact) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
