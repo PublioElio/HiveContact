a68b574913e82b493d908dca397fb7df5c2c5209
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2025-06-02 08:42:15 +0200

    added ResponseEntity to update Contact

--- src/main/java/controller/ContactsController.java
+++ src/main/java/controller/ContactsController.java
@@ -2,8 +2,9 @@ package controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -42,8 +43,9 @@ public class ContactsController {
 	}
 
 	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-	public void updateContact(@RequestBody Contact contact) {
+	public ResponseEntity<Contact> updateContact(@RequestBody Contact contact) {
 		service.updateContact(contact);
+		return ResponseEntity.ok(contact);
 	}
 
 	@DeleteMapping("/{id}")
