1d8bcbe1c8f9cc2252dd5a8beb4b3dc18e724faa
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2025-06-02 09:07:05 +0200

    return null in updatedContact

--- src/main/java/controller/ContactsController.java
+++ src/main/java/controller/ContactsController.java
@@ -47,12 +47,12 @@ public class ContactsController {
 	}
 
 	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<?> updateContact(@RequestBody Contact contact) {
+	public ResponseEntity<Contact> updateContact(@RequestBody Contact contact) {
 	    Contact updatedContact = service.getContact(contact.getIdContact());
 	    if (updatedContact != null)
 	        return ResponseEntity.ok(contact);
 	    else
-	        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Contacto no encontrado");
+	    	return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 	}
 
 	@DeleteMapping("/{id}")
