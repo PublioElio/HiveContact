c0e2d8e529a7d78734f274990229959e1f5f7453
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2025-06-02 08:29:30 +0200

    add contact method fixed

--- src/main/java/controller/ContactsController.java
+++ src/main/java/controller/ContactsController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,5 +32,8 @@ public class ContactsController {
 	}
 	
 	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String addContact(@RequestBody Contact contact) {
+		
+	}
 
 }
