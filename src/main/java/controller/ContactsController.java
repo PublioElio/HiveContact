0e6d57ea6ec3b0b0111237f1cdf6337af3e4fb51
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2025-06-02 08:19:46 +0200

    finished Get Contacts method in controller

--- src/main/java/controller/ContactsController.java
+++ src/main/java/controller/ContactsController.java
@@ -3,6 +3,7 @@ package controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +18,9 @@ public class ContactsController {
 	@Autowired
 	ContactsService service;
 	
-	@GetMapping(value)
+	@GetMapping(produces=MediaType.APPLICATION_JSON_VALUE)
 	public List<Contact> getAllContacts() {
-
+		return service.getAllContacts();
 	}
 
 }
