9f3555ef2a267da1a1448a687e3dc09f10701a46
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2025-06-02 08:15:42 +0200

    getAllContacts method in Controller

--- src/main/java/controller/ContactsController.java
+++ src/main/java/controller/ContactsController.java
@@ -1,9 +1,20 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+import service.ContactsService;
 
 @RestController
 public class ContactsController {
+	
+	@Autowired
+	ContactsService service;
+	
+	@GetMapping()
+	public List<Contact> getAllContacts(){
+		
+	}
 	
 
 }
