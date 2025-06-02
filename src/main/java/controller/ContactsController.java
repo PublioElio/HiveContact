a8b598618e40b2bd7b2e951d518ece50ecd61bc7
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2025-06-02 08:17:59 +0200

    Added new annotations in Controller

--- src/main/java/controller/ContactsController.java
+++ src/main/java/controller/ContactsController.java
@@ -4,18 +4,20 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import model.Contact;
 import service.ContactsService;
 
 @RestController
+@RequestMapping("/contacts")
 public class ContactsController {
 	
 	@Autowired
 	ContactsService service;
 	
-	@GetMapping()
+	@GetMapping(value)
 	public List<Contact> getAllContacts() {
 
 	}
