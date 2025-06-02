5c9e840aa880f9d34218a18d059bd859a5c6a862
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2025-06-02 08:22:48 +0200

    addContact method in Controller

--- src/main/java/controller/ContactsController.java
+++ src/main/java/controller/ContactsController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +23,11 @@ public class ContactsController {
 	@GetMapping(produces=MediaType.APPLICATION_JSON_VALUE)
 	public List<Contact> getAllContacts() {
 		return service.getAllContacts();
+	}
+	
+	@PostMapping(value="/{id}", produces=MediaType.APPLICATION_JSON_VALUE)
+	public String addContact(@RequestBody Contact contact) {
+		
 	}
 
 }
