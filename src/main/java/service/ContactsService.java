e94cce67d249e24296b555f0b31074512923bf2d
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2025-06-04 13:46:54 +0200

    Start documentation of ContactsService class

--- src/main/java/service/ContactsService.java
+++ src/main/java/service/ContactsService.java
@@ -4,7 +4,18 @@ import java.util.List;
 
 import model.Contact;
 
+/**
+ * ContactsService interface defines the contract for managing Contact entities.
+ * It provides methods to perform CRUD operations on contacts.
+ */
 public interface ContactsService {
+
+	/**
+	 * Adds a new contact to the system.
+	 * 
+	 * @param contact the Contact entity to be added
+	 * @return true if the contact was successfully added, false otherwise
+	 */
 	boolean addContact(Contact contact);
 
 	List<Contact> getAllContacts();
